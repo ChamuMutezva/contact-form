de58a919311624d0f27f49b94a4a5a678a55ce37
Chamu Mutezva <ckmutezva@gmail.com>
2024-12-28 06:05:00 +0200

    update: form errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,13 @@ const FormSchema = z.object({
     message: z.string().min(20, {
         message: "Message must be at least 20 characters.",
     }),
-    consent: z.boolean().default(false).optional(),
+    consent: z
+        .boolean({
+            required_error: "You must agree to be contacted.",
+        })
+        .refine((val) => val === true, {
+            message: "To submit this form, please consent to be contacted.",
+        }),
 });
 
 export default function Home() {
@@ -53,19 +59,22 @@ export default function Home() {
         console.log(data);
     }
     return (
-        <div className="max-w-[42rem] w-full bg-[hsl(var(--white))] rounded mx-4 my-8 p-6 sm:p-10">
-            <h2>Contact us</h2>
+        <main className="max-w-[42rem] w-full bg-[hsl(var(--white))] rounded-md mx-4 my-8 p-6 sm:p-10">
+            <h2 className="text-preset-3 font-bold">Contact us</h2>
             <Form {...form}>
                 <form
                     onSubmit={form.handleSubmit(onSubmit)}
                     className="grid gap-4 w-full sm:grid-cols-2"
                 >
+                    {/*FIRST NAME */}
                     <FormField
                         control={form.control}
                         name="firstName"
                         render={({ field }) => (
                             <FormItem className="col-span-2 sm:col-span-1">
-                                <FormLabel>First Name *</FormLabel>
+                                <FormLabel className="text-base">
+                                    First Name *
+                                </FormLabel>
                                 <FormControl>
                                     <Input placeholder="Chamu" {...field} />
                                 </FormControl>
@@ -73,12 +82,15 @@ export default function Home() {
                             </FormItem>
                         )}
                     />
+                    {/* LAST NAME */}
                     <FormField
                         control={form.control}
                         name="lastName"
                         render={({ field }) => (
                             <FormItem className="col-span-2 sm:col-span-1">
-                                <FormLabel>Last Name *</FormLabel>
+                                <FormLabel className="text-base">
+                                    Last Name *
+                                </FormLabel>
                                 <FormControl>
                                     <Input placeholder="Mutezva" {...field} />
                                 </FormControl>
@@ -86,15 +98,20 @@ export default function Home() {
                             </FormItem>
                         )}
                     />
+
+                    {/* EMAIL */}
                     <FormField
                         control={form.control}
                         name="email"
                         render={({ field }) => (
                             <FormItem className="col-span-2">
-                                <FormLabel>Email address *</FormLabel>
+                                <FormLabel className="text-base">
+                                    Email address *
+                                </FormLabel>
                                 <FormControl>
                                     <Input
                                         placeholder="example@xyz.com"
+                                        autoComplete="email"
                                         {...field}
                                     />
                                 </FormControl>
@@ -102,13 +119,15 @@ export default function Home() {
                             </FormItem>
                         )}
                     />
-
+                    {/* QUERY TYPE */}
                     <FormField
                         control={form.control}
                         name="type"
                         render={({ field }) => (
                             <FormItem className="col-span-2">
-                                <FormLabel>Query type *</FormLabel>
+                                <FormLabel className="text-base">
+                                    Query type *
+                                </FormLabel>
                                 <FormControl>
                                     <RadioGroup
                                         onValueChange={field.onChange}
@@ -117,17 +136,23 @@ export default function Home() {
                                     >
                                         <FormItem className="flex-1 items-center space-x-3 space-y-0 border rounded-md p-2">
                                             <FormControl>
-                                                <RadioGroupItem value="general" id="general"/>
+                                                <RadioGroupItem value="general" />
                                             </FormControl>
-                                            <FormLabel className="font-normal">
+                                            <FormLabel
+                                                className="font-normal text-preset-2"
+                                                htmlFor="general"
+                                            >
                                                 General enquiry
                                             </FormLabel>
                                         </FormItem>
                                         <FormItem className="flex-1 items-center space-x-3 space-y-0 border rounded-md p-2">
                                             <FormControl>
-                                                <RadioGroupItem value="support" id="support" />
+                                                <RadioGroupItem value="support" />
                                             </FormControl>
-                                            <FormLabel className="font-normal">
+                                            <FormLabel
+                                                className="font-normal text-preset-2"
+                                                htmlFor="support"
+                                            >
                                                 Support request
                                             </FormLabel>
                                         </FormItem>
@@ -138,12 +163,15 @@ export default function Home() {
                         )}
                     />
 
+                    {/* MESSAGE */}
                     <FormField
                         control={form.control}
                         name="message"
                         render={({ field }) => (
                             <FormItem className="col-span-2">
-                                <FormLabel>Message *</FormLabel>
+                                <FormLabel className="text-base">
+                                    Message *
+                                </FormLabel>
                                 <FormControl>
                                     <Textarea
                                         placeholder="Tell us a little bit about yourself"
@@ -151,41 +179,44 @@ export default function Home() {
                                         {...field}
                                     />
                                 </FormControl>
-
                                 <FormMessage />
                             </FormItem>
                         )}
                     />
 
+                    {/* CONSENT */}
                     <FormField
                         control={form.control}
                         name="consent"
                         render={({ field }) => (
-                            <FormItem className="flex flex-row col-span-2 items-start space-x-3 space-y-0 p-4">
-                                <FormControl>
-                                    <Checkbox
-                                    id="consent"
-                                        checked={field.value}
-                                        onCheckedChange={field.onChange}
-                                    />
-                                </FormControl>
-                                <div className="space-y-1 leading-none">
-                                    <FormLabel>
-                                        I consent to be contacted by the team
-                                    </FormLabel>
+                            <FormItem className="col-span-2">
+                                <div className="flex flex-row items-center space-x-3 space-y-0">
+                                    <FormControl>
+                                        <Checkbox
+                                            checked={field.value}
+                                            onCheckedChange={field.onChange}
+                                        />
+                                    </FormControl>
+                                    <div className="space-y-1 leading-none">
+                                        <FormLabel className="text-base">
+                                            I consent to be contacted by the
+                                            team
+                                        </FormLabel>
+                                    </div>
                                 </div>
+                                <FormMessage />
                             </FormItem>
                         )}
                     />
 
                     <Button
                         type="submit"
-                        className="col-span-2 bg-[hsl(var(--green-600))]"
+                        className="col-span-2 text-preset-2 py-[30px] text-[hsl(var(--white))] bg-[hsl(var(--green-600))]"
                     >
                         Submit
                     </Button>
                 </form>
             </Form>
-        </div>
+        </main>
     );
 }
